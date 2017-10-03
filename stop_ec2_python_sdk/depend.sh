a01e79398e73361676ec81511c0109ef3a1ea2d1
kraunikumar <kraunilakkam@outlook.com>
2017-10-02 21:22:52 -0600

    version 1

---
+++ stop_ec2_python_sdk/depend.sh
@@ -0,0 +1,3 @@
+
+yum install python python-devel python-pip
+python-pip install boto
---
+++ stop_ec2_python_sdk/stop.py
@@ -0,0 +1,4 @@
+import boto.ec2
+ 
+conn = boto.ec2.connect_to_region("us-west-2")
+conn.stop_instances(instance_ids=['instance-id-1','instance-id-2'])
