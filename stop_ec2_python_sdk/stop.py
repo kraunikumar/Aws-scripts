ae35783c5b753c85194449f4deef928967c21bba
root <root@ip-172-31-47-34.us-west-2.compute.internal>
2017-10-03 11:01:25 -0700

    this script was tested on amz linux

--- stop_ec2_python_sdk/stop.py
+++ stop_ec2_python_sdk/stop.py
@@ -1,4 +1,18 @@
+#/usr/bin/python
 import boto.ec2
- 
-conn = boto.ec2.connect_to_region("us-west-2")
-conn.stop_instances(instance_ids=['instance-id-1','instance-id-2'])
+import json
+import requests
+
+def id_instance():
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_id = response.text
+    return instance_id
+#print(id_instance())
+instance = id_instance()
+response = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
+
+json_data=json.loads(response.text)
+region=json_data['region']
+#print region
+conn = boto.ec2.connect_to_region(region)
+conn.stop_instances(instance_ids=[instance])
